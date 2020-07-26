a7d29379f23da34d890a94121b9ee7e31af7deed
Ridha Rahmi <ridha.rahmi13@gmail.com>
2020-07-26 18:25:27 +0100

    Updates

---
+++ precache-manifest.604325ce8c24f433439f0581314eadb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2953cab3477cd480f59866bc1f7a7118",
+    "url": "/react-app-weather/index.html"
+  },
+  {
+    "revision": "dbc7a205af47f23821c6",
+    "url": "/react-app-weather/static/css/main.49720afc.chunk.css"
+  },
+  {
+    "revision": "9678bdf81f593a4802f0",
+    "url": "/react-app-weather/static/js/2.a5a7f106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-weather/static/js/2.a5a7f106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc7a205af47f23821c6",
+    "url": "/react-app-weather/static/js/main.34b838a0.chunk.js"
+  },
+  {
+    "revision": "fd7be0c5daca31fd6d98",
+    "url": "/react-app-weather/static/js/runtime-main.cf438aa0.js"
+  }
+]);
